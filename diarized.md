6814e1f4437d002f54d1147dd848934272ae5f8d
Tricia Amheiser <tricia@taarts.net>
2022-12-06 19:19:35 -0500

    it works! Now the spaces are populated with the correct selected letters

--- diarized.md
+++ diarized.md
@@ -1,3 +1,25 @@
 placed alphabet in the game
 
-listed needed functions
+setup:
+images of snowman
+letters of alphabet for user to click
+math.floor
+
+useState to set:
+secret word
+guessed letters
+player score
+current word
+
+function to start the game:
+function to select a letter from the Alphabet
+if it's part of the word, map over the "placeholders" add it to the guessed letters by blanking it
+up the player score by one if correct and use this "score" to select the next image of the snowman
+
+Inside the game component:
+onClick function for the start button
+
+<ul><li> for split word selected and create the letter spaces "_"
+Alphabet.map to complete the word
+
+swapped randomWordLocal for secret word and then it worked
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,69 @@
 import React, { useState, useEffect } from 'react'
+import words from './words.json'
 
-// guessedLetters
-// secretWord
-// initialBlank
-// placedLetters
+import step_0 from './images/step_0.png'
+import step_1 from './images/step_1.png'
+import step_2 from './images/step_2.png'
+import step_3 from './images/step_3.png'
+import step_4 from './images/step_4.png'
+import step_5 from './images/step_5.png'
+import step_6 from './images/step_6.png'
+import step_7 from './images/step_7.png'
 
 const ALPHABET = [...Array(26)].map((_, i) => String.fromCharCode(i + 65)) // A-Z
+const randomWordLocal = words[Math.floor(Math.random() * words.length)]
 
 export function App() {
-  const [guessedLetters, setGuessedLetters] = useState('')
   const [secretWord, setSecretWord] = useState('')
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [playerScore, setPlayerScore] = useState(0)
+  const [currentWord, setCurrentWord] = useState(['_'])
 
-  async function game() {
-    const response = await fetch(
-      'https://sdg-words.herokuapp.com/api/words/random'
-    )
-    if (response.ok) {
-      const word = await response.json()
-      setSecretWord(word.toUpperCase())
-      console.log(word)
-    }
+  function game() {
+    setSecretWord(randomWordLocal.toUpperCase())
+    console.log(secretWord) //take out
   }
 
   function newGame() {
+    setGuessedLetters([''])
+    setPlayerScore(0)
+    setCurrentWord(['_'])
     game()
+  }
+
+  function clickOnLetter(letter: string) {
+    setGuessedLetters([...guessedLetters, letter])
+    console.log(letter) //take out
+
+    if (secretWord.includes(letter)) {
+      console.log('correct')
+      setPlayerScore(playerScore + 1)
+
+      for (let index = 0; index < secretWord.length; index++) {}
+
+      setPlayerScore(playerScore + 1)
+    }
+  }
+
+  function buildSnowman() {
+    switch (playerScore) {
+      case 0:
+        return step_0
+      case 1:
+        return step_1
+      case 2:
+        return step_2
+      case 3:
+        return step_3
+      case 4:
+        return step_4
+      case 5:
+        return step_5
+      case 6:
+        return step_6
+      case 7:
+        return step_7
+    }
   }
 
   return (
@@ -31,11 +71,34 @@ export function App() {
       <header>
         <h1>Let's Build a Snowman!</h1>
         <button onClick={() => newGame()}>START</button>
-        <section>snowman</section>
-        <section>wordToGuess</section>
-        <section>Alphabet</section>
-        <footer>by me</footer>
       </header>
+      <section>
+        <img className="image" src={buildSnowman()} height="300px" />
+      </section>
+      <ul>
+        {secretWord.split('').map((letter, index) => (
+          <li key={index}>{guessedLetters.includes(letter) ? letter : '_'}</li>
+        ))}
+      </ul>
+      <section>
+        {ALPHABET.map((letter) => {
+          return (
+            <button
+              key={letter}
+              onClick={function () {
+                clickOnLetter(letter)
+              }}
+              disabled={guessedLetters.includes(letter)}
+            >
+              {letter}
+            </button>
+          )
+        })}
+      </section>
+      <section>
+        <div></div>
+      </section>
+      <footer>by me</footer>
     </div>
   )
 }
